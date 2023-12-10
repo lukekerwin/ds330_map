f9814709adf89a4f7d0989126b077789ebd3c66f
Luke Kerwin <112733299+lukekerwin@users.noreply.github.com>
2023-12-10 13:34:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
 
-st.title("Geo-Visualization of Uber Trips")
+st.title("Visualization of Uber Trips")
 
 data = pd.read_csv('UberDatasetFiltered.csv')
 # START_DATE,END_DATE,CATEGORY,START,STOP,MILES,PURPOSE,START_STATE,STOP_STATE,START_LOC,STOP_LOC,START_LAT,START_LONG,STOP_LAT,STOP_LONG
@@ -25,6 +25,8 @@ purpose = st.sidebar.multiselect('Select a purpose', data['PURPOSE'].unique())
 # Category
 category = st.sidebar.multiselect('Select a category', data['CATEGORY'].unique())
 
+# Subheader
+st.subheader('Map of Trips')
 
 if page_select == 'New York City':
     st.subheader('New York City')
@@ -41,10 +43,6 @@ if page_select == 'New York City':
     map_center = [40.7128, -74.0060]
     
     map = folium.Map(location=map_center, zoom_start=10)
-    marker_cluster = MarkerCluster().add_to(map)
-    for index, row in filt_data.iterrows():
-        folium.Marker([row['LAT'], row['LON']], popup=row['PURPOSE']).add_to(marker_cluster)
-    st_folium(map, width=1200, height=600)
 
 
 if page_select == 'Texas':
@@ -62,10 +60,6 @@ if page_select == 'Texas':
     map_center = [filt_data['LAT'].mean(), filt_data['LON'].mean()]
 
     map = folium.Map(location=map_center, zoom_start=8)
-    marker_cluster = MarkerCluster().add_to(map)
-    for index, row in filt_data.iterrows():
-        folium.Marker([row['LAT'], row['LON']], popup=row['PURPOSE']).add_to(marker_cluster)
-    st_folium(map, width=1200, height=600)
 
 if page_select == 'North Carolina':
     st.subheader('North Carolina')
@@ -82,7 +76,35 @@ if page_select == 'North Carolina':
     map_center = [filt_data['LAT'].mean(), filt_data['LON'].mean()]
 
     map = folium.Map(location=map_center, zoom_start=10)
-    marker_cluster = MarkerCluster().add_to(map)
-    for index, row in filt_data.iterrows():
-        folium.Marker([row['LAT'], row['LON']], popup=row['PURPOSE']).add_to(marker_cluster)
-    st_folium(map, width=1200, height=600)
+
+marker_cluster = MarkerCluster().add_to(map)
+for index, row in filt_data.iterrows():
+    folium.Marker([row['LAT'], row['LON']], popup=row['PURPOSE']).add_to(marker_cluster)
+st_folium(map, width=1200, height=600)
+
+st.subheader('Time Popularity')
+
+# Display heatmap of the most popular times
+copy = filt_data.copy()
+copy['START_DATE'] = pd.to_datetime(copy['START_DATE'])
+copy['DAY'] = copy['START_DATE'].dt.day_name()
+copy['HOUR'] = copy['START_DATE'].dt.hour
+
+# Create a new dataframe with the start and end times
+times = copy[['DAY', 'HOUR']].copy()
+
+# Create a heatmap of the times using matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+fig = plt.figure(figsize=(12, 6))
+sns.heatmap(times.groupby(['DAY', 'HOUR']).size().unstack(), cmap='Reds')
+st.pyplot(fig)
+
+# Create a Dendrogram of the trip reasons
+st.subheader('Dendrogram of Trip Reasons (Does Not Get Filtered)')
+
+# embed image
+
+st.image('Dendrogram.png', width=1200)
+
+
